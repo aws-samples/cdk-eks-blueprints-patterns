fc052ec16bee926b91a5bd248f0db1cc0c82499c
Sebastien Allamand <sallaman@amazon.com>
2021-11-02 17:59:40 +0000

    change role for team platform

--- lib/teams/team-platform/index.ts
+++ lib/teams/team-platform/index.ts
@@ -1,12 +1,12 @@
-import { ArnPrincipal } from "@aws-cdk/aws-iam";
+import { ArnPrincipal } from '@aws-cdk/aws-iam';
 
 import { PlatformTeam } from '@aws-quickstart/ssp-amazon-eks';
 
 export class TeamPlatform extends PlatformTeam {
-    constructor(accountID: string) {
-        super({
-            name: "platform",
-            users: [new ArnPrincipal(`arn:aws:iam::${accountID}:user/superadmin`)]
-        })
-    }
+  constructor(accountID: string) {
+    super({
+      name: 'platform',
+      users: [new ArnPrincipal(`arn:aws:iam::${accountID}:role/demo3`)],
+    });
+  }
 }
