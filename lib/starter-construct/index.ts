4f805a60a893971e295be880862d40cfc2ba4956
EC2 Default User <ec2-user@ip-172-31-90-139.ec2.internal>
2021-12-15 18:19:45 +0000

    starter pipeline added

--- lib/starter-construct/index.ts
+++ lib/starter-construct/index.ts
@@ -2,15 +2,22 @@ import * as cdk from '@aws-cdk/core';
 
 // SSP Lib
 import * as ssp from '@aws-quickstart/ssp-amazon-eks'
-
+import * as team from '../teams'
 
 export default class StarterConstruct extends cdk.Construct {
     constructor(scope: cdk.Construct, id: string) {
         super(scope, id);
+        
+        // Setup platform team
+        const accountID = process.env.CDK_DEFAULT_ACCOUNT!
+        const platformTeam = new team.TeamPlatform(accountID)
 
         // Onboard teams as necessary - import lib/teams
         const teams: Array<ssp.Team> = [
-
+            platformTeam,
+            new team.TeamTroiSetup,
+            new team.TeamRikerSetup,
+            new team.TeamBurnhamSetup(scope)
         ];
 
         // Include more addons as necessary
@@ -21,9 +28,50 @@ export default class StarterConstruct extends cdk.Construct {
         const stackID = `${id}-blueprint`
         new ssp.EksBlueprint(scope, { id: stackID, addOns, teams }, {
             env: {
-                region: 'us-east-2',
+                region: 'us-east-1',
             },
         });
+        
+        // Pipeline construct
+        const account = process.env.CDK_DEFAULT_ACCOUNT;
+        const region = process.env.CDK_DEFAULT_REGION;
+        const env = { account, region };
+        const blueprint = ssp.EksBlueprint.builder()
+            .account(accountID) 
+            .region('us-east-1')
+            .addOns(
+                new ssp.AwsLoadBalancerControllerAddOn, 
+                new ssp.NginxAddOn,
+                new ssp.ArgoCDAddOn
+            )
+            .teams(
+                new team.TeamPlatform(accountID),
+                new team.TeamRikerSetup,
+                new team.TeamBurnhamSetup(scope),
+                new team.TeamTroiSetup
+            );
+            
+        // Build code pipeline and add stages
+        ssp.CodePipelineStack.builder()
+            .name("ssp-eks-workshop-pipeline")
+            .owner("aws-samples")
+            .repository({
+                repoUrl: 'ssp-eks-patterns',
+                credentialsSecretName: 'github-token',
+                targetRevision: 'young-workshop-test'
+            })
+            .stage({
+                id: 'dev',
+                stackBuilder: blueprint.clone('us-east-1')
+            })
+            .stage({
+                id: 'prod',
+                stackBuilder: blueprint.clone('us-west-2'),
+                stageProps: {
+                    manualApprovals: true
+                }
+            })
+            .build(scope, `${id}-pipeline-stack`, {env});
     }
 }
 
