9e7d4f1e12c4f8f19072b903ba0b12abacfccb8b
Mikhail Shapirov <shapirov@amazon.com>
2022-07-20 11:09:41 -0400

    upgraded library and multi-region pattern

--- lib/multi-region-construct/index.ts
+++ lib/multi-region-construct/index.ts
@@ -1,10 +1,11 @@
 // Blueprints Lib
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import { Construct } from 'constructs';
 import { utils } from '@aws-quickstart/eks-blueprints';
+import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+import { Construct } from 'constructs';
 
 // Team implementations
-import * as team from '../teams'
+import * as team from '../teams';
 const burnhamManifestDir = './lib/teams/team-burnham/'
 const rikerManifestDir = './lib/teams/team-riker/'
 const teamManifestDirList = [burnhamManifestDir,rikerManifestDir]
@@ -32,6 +33,11 @@ export default class MultiRegionConstruct {
         
         const blueprint = blueprints.EksBlueprint.builder()
             .account(process.env.CDK_DEFAULT_ACCOUNT!)
+            .clusterProvider(new blueprints.MngClusterProvider({
+                version: KubernetesVersion.V1_21,
+                desiredSize: 2,
+                maxSize: 3
+            }))
             .addOns( new blueprints.AwsLoadBalancerControllerAddOn,
                 new blueprints.NginxAddOn,
                 new blueprints.CalicoOperatorAddOn,
