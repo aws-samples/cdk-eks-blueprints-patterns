94c2dfadc4940a6b293a90cff4677451dfc8b0f1
EC2 Default User <ec2-user@ip-10-1-1-13.us-west-2.compute.internal>
2022-08-26 03:24:57 +0000

    add kubeflow example

---
+++ lib/kubeflow-construct/index.ts
@@ -0,0 +1,28 @@
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints'
+import { KubeflowAddOn } from 'eks-blueprints-cdk-kubeflow-ext';
+
+
+export default class KubeflowConstruct {
+    constructor(scope: Construct, id: string) {
+        // AddOns for the cluster
+        const stackId = `${id}-blueprint`;
+
+        blueprints.EksBlueprint.builder()
+            .account(process.env.CDK_DEFAULT_ACCOUNT!)
+            .region(process.env.CDK_DEFAULT_REGION)
+            .addOns( new blueprints.MetricsServerAddOn(),
+                new blueprints.ClusterAutoScalerAddOn(),
+                new blueprints.AwsLoadBalancerControllerAddOn(),
+                new blueprints.VpcCniAddOn(),
+                new blueprints.CoreDnsAddOn(),
+                new blueprints.KubeProxyAddOn(),
+                new blueprints.EbsCsiDriverAddOn(),
+                new KubeflowAddOn({
+                     namespace: 'kubeflow-pipelines'
+                 })
+            )
+            .teams()// add teams here)
+            .build(scope, stackId);
+    }
+}
