29eea069c216f10eaa2cc06282c6262e50cd2869
Elamaran Shanmugam <elamaran.shan@gmail.com>
2022-10-31 14:50:48 +0000

    Observability Accelarator

--- lib/observability-accelarator/observabilityaccelarator.ts
+++ lib/observability-accelarator/observabilityaccelarator.ts
@@ -3,60 +3,40 @@ import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import AmpMonitoringConstruct from '../amp-monitoring';
-import CloudWatchMonitoringConstruct from '../cloudwatch-monitoring';
-import { AmgIamSetupStack, AmgIamSetupStackProps } from './amg-iam-setup';
+import { AmgSetupStack, AmgSetupStackProps } from './amg-setup';
 import { AmpIamSetupStack } from './amp-iam-setup';
-import { CloudWatchIamSetupStack } from './cloudwatch-iam-setup';
 
 const logger = blueprints.utils.logger;
 
 /**
- * Function relies on a secret called "cdk-context" defined in the target region (pipeline account must have it)
+ * Function relies on a secret called "cdk-context" defined in the target region
  * @returns 
  */
-export async function populateAccountWithContextDefaults(): Promise<PipelineMultiEnvMonitoringProps> {
+export async function populateAccountWithContextDefaults(): Promise<ObservabilityAccelaratorProps> {
     // Populate Context Defaults for all the accounts
-    const cdkContext = JSON.parse(await blueprints.utils.getSecretValue('cdk-context', 'us-east-1'))['context'] as PipelineMultiEnvMonitoringProps;
+    const cdkContext = JSON.parse(await blueprints.utils.getSecretValue('cdk-context', 'us-east-1'))['context'] as ObservabilityAccelaratorProps;
     logger.debug(`Retrieved CDK context ${JSON.stringify(cdkContext)}`);
     return cdkContext;
 }
 
-export interface PipelineMultiEnvMonitoringProps {
+export interface ObservabilityAccelaratorProps {
     /**
-     * Production workload environment (account/region) #1 
+     * Environment (account/region) where observability accelarator will be created
      */
-    prodEnv1: cdk.Environment;
-
-    /**
-     * Production workload environment (account/region) #2
-     */
-    prodEnv2: cdk.Environment;
-
-    /**
-     * Environment (account/region) where pipeline will be running (generally referred to as CICD account)
-     */
-    pipelineEnv: cdk.Environment;
-
-    /**
-     * Environment (account/region) where monitoring dashboards will be configured.
-     */
-    monitoringEnv: cdk.Environment;
+    observabilityEnv: cdk.Environment;
 }
 
 /**
- * Main multi-account monitoring pipeline.
+ * Observability Accelarator.
  */
-export class PipelineMultiEnvMonitoring {
+export class ObservabilityAccelarator {
 
     async buildAsync(scope: Construct) {
         const context = await populateAccountWithContextDefaults();
         // environments IDs consts
-        const PROD1_ENV_ID = `eks-prod1-${context.prodEnv1.region}`
-        const PROD2_ENV_ID = `eks-prod2-${context.prodEnv2.region}`
-        const MON_ENV_ID = `central-monitoring-${context.monitoringEnv.region}`
+        const ENV_ID = `eks-observability-${context.observabilityEnv.region}`
 
-        const blueprintAmp = new AmpMonitoringConstruct().create(scope, context.prodEnv1.account, context.prodEnv1.region);
-        const blueprintCloudWatch = new CloudWatchMonitoringConstruct().create(scope, context.prodEnv2.account, context.prodEnv2.region);
+        const blueprintAmp = new AmpMonitoringConstruct().create(scope, context.observabilityEnv.account, context.observabilityEnv.region);
 
         // Argo configuration per environment
         const prodArgoAddonConfig = createArgoAddonConfig('prod', 'https://github.com/aws-samples/eks-blueprints-workloads.git');
@@ -65,17 +45,17 @@ export class PipelineMultiEnvMonitoring {
         const gitOwner = 'aws-samples';
         const gitRepositoryName = 'cdk-eks-blueprints-patterns';
 
-        const amgIamSetupStackProps: AmgIamSetupStackProps = {
+        const amgSetupStackProps: AmgSetupStackProps = {
             roleName: "amgWorkspaceIamRole",
-            accounts: [context.prodEnv1.account!, context.prodEnv2.account!],
+            accounts: [context.observabilityEnv.account!],
             env: {
-                account: context.monitoringEnv.account!,
-                region: context.monitoringEnv.region!
+                account: context.observabilityEnv.account!,
+                region: context.observabilityEnv.region!
             }
         };
 
         blueprints.CodePipelineStack.builder()
-            .name("multi-account-central-pipeline")
+            .name("observability-accelarator")
             .owner(gitOwner)
             .codeBuildPolicies([ 
                 new iam.PolicyStatement({
@@ -98,11 +78,11 @@ export class PipelineMultiEnvMonitoring {
                 id: "prod-test",
                 stages: [
                     {
-                        id: PROD1_ENV_ID,
+                        id: ENV_ID,
                         stackBuilder: blueprintAmp
-                            .clone(context.prodEnv1.region, context.prodEnv1.account)
+                            .clone(context.observabilityEnv.region, context.observabilityEnv.account)
                             .addOns(new blueprints.NestedStackAddOn({
-                                builder: AmpIamSetupStack.builder("ampPrometheusDataSourceRole", context.monitoringEnv.account!),
+                                builder: AmpIamSetupStack.builder("ampPrometheusDataSourceRole", context.observabilityEnv.account!),
                                 id: "amp-iam-nested-stack"
                             }))
                             .addOns(
@@ -110,29 +90,17 @@ export class PipelineMultiEnvMonitoring {
                             )
                     },
                     {
-                        id: PROD2_ENV_ID,
-                        stackBuilder: blueprintCloudWatch
-                            .clone(context.prodEnv2.region, context.prodEnv2.account)
-                            .addOns(new blueprints.NestedStackAddOn({
-                                builder: CloudWatchIamSetupStack.builder("cloudwatchDataSourceRole", context.monitoringEnv.account!),
-                                id: "cloudwatch-iam-nested-stack"
-                            }))
-                            .addOns(
-                                prodArgoAddonConfig,
-                            )
-                    },
-                    {
-                        id: MON_ENV_ID,
+                        id: ENV_ID,
                         stackBuilder: <blueprints.StackBuilder>{
                             build(scope: Construct): cdk.Stack {
-                                return new AmgIamSetupStack(scope, "amg-iam-setup", amgIamSetupStackProps);
+                                return new AmgSetupStack(scope, "amg-setup", amgSetupStackProps);
                             }
                         }
                     },
                 ],
             })
-            .build(scope, "multi-account-central-pipeline", {
-                env: context.pipelineEnv
+            .build(scope, "observability-accelarator", {
+                env: context.observabilityEnv
             });
     }
 }
