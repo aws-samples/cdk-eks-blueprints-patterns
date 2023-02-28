04d6a56d5fe79ef85f7ed0581ccc13bbe621ce77
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-02-28 16:05:17 -0600

    Adding ArgoCD to the cluster

--- lib/security/data-at-rest-encryption/index.ts
+++ lib/security/data-at-rest-encryption/index.ts
@@ -1,23 +1,18 @@
 import * as blueprints from "@aws-quickstart/eks-blueprints";
 import {
   ArgoCDAddOn,
-  AwsLoadBalancerControllerAddOn,
-  CertManagerAddOn,
-  ClusterAutoScalerAddOn,
-  CoreDnsAddOn,
   EbsCsiDriverAddOn,
   EksBlueprint,
   GlobalResources,
-  KubeProxyAddOn,
-  MetricsServerAddOn,
-  VpcCniAddOn,
 } from "@aws-quickstart/eks-blueprints";
 import { Construct } from "constructs";
 import { SECRET_ARGO_ADMIN_PWD } from "../../multi-region-construct";
 
 // const gitUrl = "https://github.com/aws-samples/eks-blueprints-workloads.git";
+// const targetRevision = "main";
 const gitUrl =
   "https://github.com/aliaksei-ivanou/eks-blueprints-workloads.git";
+const targetRevision = "ebs-encryption-at-rest";
 
 export default class EksEncryptionConstruct {
   build(scope: Construct, id: string) {
@@ -30,20 +25,20 @@ export default class EksEncryptionConstruct {
           kmsKeys: [
             blueprints.getNamedResource(blueprints.GlobalResources.KmsKey),
           ],
+        }),
+        new ArgoCDAddOn({
+          bootstrapRepo: {
+            repoUrl: gitUrl,
+            targetRevision: targetRevision,
+            path: "security/envs/dev",
+          },
+          bootstrapValues: {
+            kmsKey: blueprints.getNamedResource(
+              blueprints.GlobalResources.KmsKey
+            ),
+          },
+          adminPasswordSecretName: SECRET_ARGO_ADMIN_PWD,
         })
-        // new ArgoCDAddOn({
-        //   bootstrapRepo: {
-        //     repoUrl: gitUrl,
-        //     targetRevision: "main",
-        //     path: "security/envs/dev",
-        //   },
-        //   bootstrapValues: {
-        //     kmsKey: blueprints.getNamedResource(
-        //       blueprints.GlobalResources.KmsKey
-        //     ),
-        //   },
-        //   adminPasswordSecretName: SECRET_ARGO_ADMIN_PWD,
-        // })
       )
       .teams() // add teams here
       .build(scope, stackId);
