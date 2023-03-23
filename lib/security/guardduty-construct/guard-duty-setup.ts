f8fb944c97b71141292ec7530cd29ea300ad6a4a
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-03-23 14:49:16 -0500

    Verifying if the GuardDuty Detector is already created in the refion

--- lib/security/guardduty-construct/guard-duty-setup.ts
+++ lib/security/guardduty-construct/guard-duty-setup.ts
@@ -1,19 +1,23 @@
 import {
-  aws_guardduty as guardduty,
+  aws_guardduty,
   aws_events as events,
   aws_events_targets as eventTargets,
   aws_sns as sns,
   aws_sns_subscriptions as subs,
+  NestedStack,
+  NestedStackProps,
+  CfnOutput,
 } from "aws-cdk-lib";
-import { NestedStack, NestedStackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as cr from "aws-cdk-lib/custom-resources";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as blueprints from "@aws-quickstart/eks-blueprints";
 
 export class GuardDutySetupStack extends NestedStack {
   public static builder(
     environmentName: string,
     email: string,
-    dataSources?: guardduty.CfnDetector.CFNDataSourceConfigurationsProperty
+    dataSources?: aws_guardduty.CfnDetector.CFNDataSourceConfigurationsProperty
   ): blueprints.NestedStackBuilder {
     return {
       build(scope: Construct, id: string, props: NestedStackProps) {
@@ -35,15 +39,40 @@ export class GuardDutySetupStack extends NestedStack {
     props: NestedStackProps,
     environmentName: string,
     email: string,
-    dataSources?: guardduty.CfnDetector.CFNDataSourceConfigurationsProperty
+    dataSources?: aws_guardduty.CfnDetector.CFNDataSourceConfigurationsProperty
   ) {
     super(scope, id, props);
 
-    // Enable GuardDuty in the AWS region, to detect security issues and include optional properties to configure.
-    new guardduty.CfnDetector(this, id + "GuardDutyDetector", {
-      enable: true,
-      dataSources: dataSources,
-    });
+    const isGuardDutyEnabled = new cr.AwsCustomResource(
+      this,
+      "IsGuardDutyEnabled",
+      {
+        onUpdate: {
+          service: "GuardDuty",
+          action: "listDetectors",
+          parameters: {},
+          physicalResourceId: cr.PhysicalResourceId.of("IsGuardDutyEnabled"),
+        },
+        policy: cr.AwsCustomResourcePolicy.fromStatements([
+          new iam.PolicyStatement({
+            actions: ["guardduty:ListDetectors"],
+            resources: ["*"],
+          }),
+        ]),
+      }
+    );
+
+    if (isGuardDutyEnabled.getResponseField("DetectorIds").length > 0) {
+      new CfnOutput(this, "GuardDutyDetectorExists", {
+        value:
+          "GuardDuty is already enabled in this region. Skipping (assuming it's managed in some other way)...",
+      });
+    } else {
+      new aws_guardduty.CfnDetector(this, id + "GuardDutyDetector", {
+        enable: true,
+        dataSources: dataSources,
+      });
+    }
 
     // Configure GuardDuty to email any security findings
     const guardDutyTopic = new sns.Topic(
