54a80fa4b066c96fc032447584b7bfb3ceee6061
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-03-23 10:29:40 -0500

    Adding ArgoCD to run a sample GitOps workload

--- lib/security/guardduty-construct/index.ts
+++ lib/security/guardduty-construct/index.ts
@@ -1,9 +1,13 @@
 import { GuardDutySetupStack } from "./guard-duty-setup";
 import { Construct } from "constructs";
 import * as blueprints from "@aws-quickstart/eks-blueprints";
+import { SECRET_ARGO_ADMIN_PWD } from "../../multi-region-construct";
 
 const environmentName = "main";
 const email = "your-email@example.com";
+
+const gitUrl = "https://github.com/aws-samples/eks-blueprints-workloads.git";
+const targetRevision = "main";
 
 export default class GuardDutyNotifier {
   build(scope: Construct, id: string) {
@@ -21,6 +25,14 @@ export default class GuardDutyNotifier {
             s3Logs: { enable: true },
           }),
           id: "guardduty-nested-stack",
+        }),
+        new blueprints.ArgoCDAddOn({
+          bootstrapRepo: {
+            repoUrl: gitUrl,
+            targetRevision: targetRevision,
+            path: "teams/team-danger/dev",
+          },
+          adminPasswordSecretName: SECRET_ARGO_ADMIN_PWD,
         })
       )
       .teams()
