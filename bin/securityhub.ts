d9e75af30f468604f263df7b60d45f4e93559456
sbollers <sb@intervein.com>
2023-05-19 19:28:51 +0000

    Created securityhub pattern.

--- bin/securityhub.ts
+++ bin/securityhub.ts
@@ -1,2 +1,5 @@
+import { configureApp } from '../lib/common/construct-utils';
 import { SecurityHubStackSetup } from '../lib/security/securityhub-construct/securityhub-setup';
+
+const app = configureApp();
 new SecurityHubStackSetup(app, 'securityhub-setup');
