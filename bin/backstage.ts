41654e55b12e45104261dc39ea0f4e7967fbd029
Riccardo Freschi <freschri@amazon.com>
2023-05-20 07:44:02 +0100

    refactored following refactor from upstream

---
+++ bin/backstage.ts
@@ -0,0 +1,24 @@
+import { BackstageConstruct, BackstageConstructProps } from '../lib/backstage-construct';
+import { configureApp } from '../lib/common/construct-utils';
+
+const app = configureApp();
+
+const backstageConstructProps = {
+    account: '...',
+    region: '...',
+    namespace: "backstage",
+    backstageImageRegistry:  "e.g.: {account}.dkr.ecr.{region}.amazonaws.com",
+    backstageImageRepository: "e.g.: backstage",
+    backstageImageTag: "e.g.: latest",
+    parentDomain: "e.g.: example.com",
+    backstageLabel: "e.g.: backstage6",
+    hostedZoneId: "Hosted zone ID (format: 20x chars/numbers)",
+    certificateResourceName: "e.g.: backstage-certificate",
+    databaseResourceName: "e.g.: backstage-database",
+    databaseInstancePort: 5432,
+    databaseSecretResourceName: "e.g.: backstage-database-credentials",
+    username: "e.g.: postgres",
+    databaseSecretTargetName: "e.g.: backstage-database-secret",
+  } as BackstageConstructProps;
+  
+  new BackstageConstruct(app, 'backstage-stack', backstageConstructProps);
