ff3dabccf67952f10005b9b1965a2379da7fcdb0
Young <youngjeong46@gmail.com>
2023-05-25 21:24:49 -0500

    added the right permissions for Fluent Bit

--- lib/aws-batch-on-eks-construct/index.ts
+++ lib/aws-batch-on-eks-construct/index.ts
@@ -1,10 +1,25 @@
 import { Construct } from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints'
 import { BatchEksTeam } from '@aws-quickstart/eks-blueprints';
+import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 
 export default class BatchOnEKSConstruct {
     build(scope: Construct, id: string, teams: BatchEksTeam[]) {
         
+        const batchIamPolicy = new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                "cloudwatch:PutMetricData",
+                "ec2:DescribeVolumes",
+                "ec2:DescribeTags",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams",
+                "logs:DescribeLogGroups",
+                "logs:CreateLogStream",
+                "logs:CreateLogGroup"
+            ],
+            resources: ["*"],
+        })
         const stackID = `${id}-blueprint`
         blueprints.EksBlueprint.builder()
             .account(process.env.CDK_DEFAULT_ACCOUNT!)
@@ -12,12 +27,12 @@ export default class BatchOnEKSConstruct {
             .addOns(
                 new blueprints.AwsBatchAddOn(), 
                 new blueprints.AwsForFluentBitAddOn({
-                    version: '0.1.23',
+                    iamPolicies:[batchIamPolicy],
                     values: {
                         cloudWatch: {
                             enabled: true,
                             region: process.env.CDK_DEFAULT_REGION!,
-                            logGroupName: 'aws-batch-for-eks-logs'
+                            logGroupName: '/aws/batch/batch-team-a-logs'
                         },
                         tolerations: [{
                             "key": "batch.amazonaws.com/batch-node", "operator": "Exists"
