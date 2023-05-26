158e7964157efc54403bd811ee62bc2b7d5e7ed1
Young <youngjeong46@gmail.com>
2023-05-25 20:02:40 -0500

    merge issue resolved

--- lib/teams/index.ts
+++ lib/teams/index.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-export { TeamBurnhamSetup } from './team-burnham'
-export { TeamPlatform } from './team-platform'
-export { TeamRikerSetup } from './team-riker'
-export { TeamTroiSetup } from './team-troi'
-export { dataTeam } from './team-emr-on-eks';
-export { batchTeam } from './team-batch';
-
-=======
 export { TeamBurnhamSetup } from "./team-burnham";
 export { TeamPlatform } from "./team-platform";
 export { TeamRikerSetup } from "./team-riker";
 export { TeamTroiSetup } from "./team-troi";
 export { dataTeam } from "./team-emr-on-eks";
 export { TeamScan } from "./team-scan";
->>>>>>> main
+export { batchTeam } from './team-batch';
