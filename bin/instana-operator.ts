8703b5259742eb1bdf5b9e61b61d28fbacefe086
Ela AWS <51791117+elamaran11@users.noreply.github.com>
2023-06-12 10:23:20 -0400

    Revert "Instana addon"

--- bin/instana-operator.ts
+++
@@ -1,8 +0,0 @@
-import { configureApp, errorHandler } from '../lib/common/construct-utils';
-import InstanaConstruct from '../lib/instana-construct';
-
-const app = configureApp();
-
-new InstanaConstruct().buildAsync(app).catch(() => {
-    errorHandler(app, "Instana pattern is not setup due to invalid parameters/environment variables");
-});
