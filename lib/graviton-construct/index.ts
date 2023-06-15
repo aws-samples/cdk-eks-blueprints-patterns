23f58deb8fa83e7bae6f697e97d12662a7377b08
Zach Jacobson <jacobsonzacharym@gmail.com>
2023-06-15 13:37:41 -0700

    add compatible addons to pattern for mvp

--- lib/graviton-construct/index.ts
+++ lib/graviton-construct/index.ts
@@ -1,26 +1,35 @@
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as eks from "aws-cdk-lib/aws-eks";
-
 import * as blueprints from "@aws-quickstart/eks-blueprints";
-
 export default class GravitonConstruct {
   build(scope: Construct, id: string) {
+    const account = process.env.CDK_DEFAULT_ACCOUNT!;
+    const region = process.env.CDK_DEFAULT_REGION!;
     const stackID = `${id}-blueprint`;
 
-    const mngProps = {
-      version: eks.KubernetesVersion.V1_25,
+    const mngProps: blueprints.MngClusterProviderProps = {
+      version: eks.KubernetesVersion.V1_26,
       instanceTypes: [new ec2.InstanceType("m7g.large")],
       amiType: eks.NodegroupAmiType.AL2_ARM_64,
+      desiredSize: 3,
+      minSize: 2,
+      maxSize: 6,
     };
 
+    const addOns: Array<blueprints.ClusterAddOn> = [
+      new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+      new blueprints.addons.ClusterAutoScalerAddOn(),
+      new blueprints.addons.VpcCniAddOn(),
+      new blueprints.addons.KubeStateMetricsAddOn(),
+    ];
     const clusterProvider = new blueprints.MngClusterProvider(mngProps);
 
     blueprints.EksBlueprint.builder()
-      .account(process.env.CDK_DEFAULT_ACCOUNT!)
-      .region(process.env.CDK_DEFAULT_REGION)
+      .account(account)
+      .region(region)
       .clusterProvider(clusterProvider)
-      .addOns()
+      .addOns(...addOns)
       .build(scope, stackID);
   }
 }
