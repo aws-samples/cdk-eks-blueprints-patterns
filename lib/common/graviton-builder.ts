62e1b531d6ac7eb8b1e2cb4e792423983027b702
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-07-06 21:35:48 -0400

    Graviton Pattern Tracking

--- lib/common/graviton-builder.ts
+++ lib/common/graviton-builder.ts
@@ -6,52 +6,117 @@ import * as eks from "aws-cdk-lib/aws-eks";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export interface GravitonOptions {
-    KubernetesVersion: string,
+    kubernetesVersion: eks.KubernetesVersion,
     instanceClass: ec2.InstanceClass,
-    instanceSize: ec2.InstanceSize,
-    addIstioAddons: boolean,
-    addMetricsAddons: boolean,
-    addSecretAddons: boolean,
-    addCalicoAddon: boolean
+    instanceSize: ec2.InstanceSize
 }
 
 export class GravitonBuilder extends blueprints.BlueprintBuilder {
 
+
+    public addIstioBaseAddOn(props?: blueprints.IstioBaseAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.IstioBaseAddOn(props)
+        );
+    }
+
+    public addIstioControlPlaneAddOn(props?: blueprints.IstioControlPlaneAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.IstioControlPlaneAddOn(props)
+        );
+    }
+
+    public addKubeStateMetricsAddOn(props?: blueprints.KubeStateMetricsAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.KubeStateMetricsAddOn(props)
+        );
+    }
+
+    public addMetricsServerAddOn() : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.MetricsServerAddOn()
+        );
+    }
+
+    public addPrometheusNodeExporterAddOn(props?: blueprints.PrometheusNodeExporterAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.PrometheusNodeExporterAddOn(props)
+        );
+    }
+
+    public addExternalsSecretsAddOn(props?: blueprints.ExternalsSecretsAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.ExternalsSecretsAddOn(props)
+        );
+    }
+
+    public addSecretsStoreAddOn(props?: blueprints.SecretsStoreAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.SecretsStoreAddOn(props)
+        );
+    }
+
+    public addCalicoOperatorAddOn(props?: blueprints.CalicoOperatorAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.CalicoOperatorAddOn(props)
+        );
+    }
+
+    public addCertManagerAddOn(props?: blueprints.CertManagerAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.CertManagerAddOn(props)
+        );
+    }
+
+    public addAdotCollectorAddOn() : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.AdotCollectorAddOn()
+        );
+    }
+
+    public addAmpAddOn(props: blueprints.AmpAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.AmpAddOn(props)
+        );
+    }
+
+    public addCloudWatchLogsAddOn(props: blueprints.CloudWatchLogsAddonProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.CloudWatchLogsAddon(props)
+        );
+    }
+
+    public addEfsCsiDriverAddOn(props?: blueprints.EfsCsiDriverProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.EfsCsiDriverAddOn(props)
+        );
+    } 
+
+    public addFluxCDAddOn(props?: blueprints.FluxCDAddOnProps) : GravitonBuilder {  
+        return this.addOns(
+            new blueprints.addons.FluxCDAddOn(props)
+        );
+    }  
+
+    public addGrafanaOperatorAddOn(props?: blueprints.GrafanaOperatorAddonProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.GrafanaOperatorAddon(props)
+        );
+    }  
+
+    public addXrayAdotAddOn(props?: blueprints.XrayAdotAddOnProps) : GravitonBuilder {
+        return this.addOns(
+            new blueprints.addons.XrayAdotAddOn(props)
+        );
+    }
+
     public static builder(options: GravitonOptions): GravitonBuilder {
         const builder = new GravitonBuilder();
 
-        if (options.addIstioAddons) {
-            builder.addOns(
-                new blueprints.addons.IstioBaseAddOn(),
-                new blueprints.addons.IstioControlPlaneAddOn(),
-            );
-        }
-
-        if (options.addMetricsAddons) {
-            builder.addOns(
-                new blueprints.addons.KubeStateMetricsAddOn(),
-                new blueprints.addons.MetricsServerAddOn(),
-                new blueprints.addons.PrometheusNodeExporterAddOn(),
-            );
-        }
-
-        if (options.addSecretAddons) {
-            builder.addOns(
-                new blueprints.addons.ExternalsSecretsAddOn(),
-                new blueprints.addons.SecretsStoreAddOn(),
-            );
-        }
-
-        if (options.addCalicoAddon) {
-            builder.addOns(
-                new blueprints.addons.CalicoOperatorAddOn(),
-            );
-        }
-        
         builder
             .clusterProvider(
                 new blueprints.MngClusterProvider({
-                    version: eks.KubernetesVersion.of(options.KubernetesVersion),
+                    version: options.kubernetesVersion,
                     instanceTypes: [new ec2.InstanceType(`${options.instanceClass}.${options.instanceSize}`)],
                     amiType: eks.NodegroupAmiType.AL2_ARM_64,
                     desiredSize: 3,
