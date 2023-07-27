0ea3e7719e40b53d5f4dbd3a76cf75a44ba1c704
Zach Jacobson <jacobsonzacharym@gmail.com>
2023-07-27 14:54:57 -0700

    make karpenter work

--- lib/karpenter-construct/index.ts
+++ lib/karpenter-construct/index.ts
@@ -12,22 +12,13 @@ export default class KarpenterConstruct {
                 { key: 'node.kubernetes.io/instance-type', op: 'In', vals: ['m5.large'] },
                 { key: 'topology.kubernetes.io/zone', op: 'NotIn', vals: ['us-west-2c']},
                 { key: 'kubernetes.io/arch', op: 'In', vals: ['amd64','arm64']},
-                { key: 'karpenter.sh/capacity-type', op: 'In', vals: ['spot','on-demand']},
+                { key: 'karpenter.sh/capacity-type', op: 'In', vals: ['on-demand']},
             ],
             subnetTags: {
                 "Name": "karpenter-blueprint/karpenter-blueprint-vpc/PrivateSubnet1",
             },
             securityGroupTags: {
                 "kubernetes.io/cluster/karpenter-blueprint": "owned",
-            },
-            taints: [{
-                key: "workload",
-                value: "test",
-                effect: "NoSchedule",
-            }],
-            amiFamily: "AL2",
-            amiSelector: {
-                "karpenter.sh/discovery/MyClusterName": '*',
             },
             consolidation: { enabled: true },
             ttlSecondsUntilExpired: 2592000,
@@ -39,7 +30,11 @@ export default class KarpenterConstruct {
     EksBlueprint.builder()
             .account(account)
             .region(region)
-            .addOns(addon)
+            .addOns(
+                addon,
+                new blueprints.addons.SSMAgentAddOn(),
+                new blueprints.addons.MetricsServerAddOn(),
+            )
             .build(scope, stackID);
 
 }
