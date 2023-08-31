e59b11c9abc5b105f1872ada8f744003c909a011
Math Bruneau <brunemat@amazon.com>
2023-08-31 16:50:45 -0400

    Added karpenter provisioner for Windows2022 Change to ManagedNodeGroup to be more flexible Keep imdvs2 false only for linux

--- lib/windows-construct/index.ts
+++ lib/windows-construct/index.ts
@@ -3,8 +3,7 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as eks from "aws-cdk-lib/aws-eks";
 import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
-import { WindowsBuilder, WindowsOptions } from '../common/windows-builder';
-// import { WindowsVpcCni } from "./vpc-cni";
+import { WindowsBuilder, WindowsOptions, WindowsVpcCni } from "@aws-quickstart/eks-blueprints";
 
 export default class WindowsConstruct {
     build(scope: Construct, id: string) {
@@ -22,10 +21,10 @@ export default class WindowsConstruct {
 
         const options: WindowsOptions = {
             kubernetesVersion: eks.KubernetesVersion.of("1.27"),
-            instanceClass: ec2.InstanceClass.M5,
-            instanceSize: ec2.InstanceSize.XLARGE4,
-            desiredNodeSize: 2,
-            minNodeSize: 2,
+            instanceClass: ec2.InstanceClass.T3,
+            instanceSize: ec2.InstanceSize.MEDIUM,
+            desiredNodeCount: 1,
+            minNodeSize: 1,
             maxNodeSize: 3,
             blockDeviceSize: 50,
             noScheduleForWindowsNodes: true,
@@ -33,21 +32,48 @@ export default class WindowsConstruct {
                 "Name": "blueprints-windows-eks-cluster",
                 "Type": "generic-windows-cluster"
             },
-            genericNodeGroupTags: {
-                "Name": "Mng-linux",
-                "Type": "Managed-linux-Node-Group",
-                "LaunchTemplate": "Linux-Launch-Template",
+            genericNodeGroupOptions: {
+                nodegroupName: "Mng-linux",
+                tags: {
+                    "kubernetes.io/cluster/windows-eks-blueprint": "owned"
+                },
             },
-            windowsNodeGroupTags: {
-                "Name": "Managed-Node-Group",
-                "Type": "Windows-Node-Group",
-                "LaunchTemplate": "WindowsLT",
-                "kubernetes.io/cluster/windows-eks-blueprint": "owned"  
+            windowsNodeGroupOptions: {
+                nodegroupName: "Mng-windows",
+                tags: {
+                    "kubernetes.io/cluster/windows-eks-blueprint": "owned"
+                }
             }
         };
 
+        const karpenterAddon = new blueprints.addons.KarpenterAddOn({
+            version: "v0.31.3",
+            requirements: [
+                { key: 'kubernetes.io/os', op: 'In', vals: ['windows']},
+            ],
+            subnetTags: {
+                "Name": `${stackID}/${stackID}-vpc/Private*`,
+            },
+            securityGroupTags: {
+                [`kubernetes.io/cluster/${stackID}`]: "owned",
+            },
+            consolidation: { enabled: true },
+            ttlSecondsUntilExpired: 2592000,
+            weight: 20,
+            interruptionHandling: true,
+            taints: [
+                {
+                    key: "os",
+                    value: "windows",
+                    effect: "NoSchedule"
+                }
+            ],
+            amiFamily: "Windows2022"
+        });
+
         const addOns: Array<blueprints.ClusterAddOn> = [
-            // new WindowsVpcCni()
+            new WindowsVpcCni(),
+            karpenterAddon
         ];
 
         WindowsBuilder.builder(options)
