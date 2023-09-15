765071b295f76878add58b4351bc3cb8b8facb3c
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-09-15 14:00:22 -0400

    Generative AI fix usage tracking

--- lib/generative-ai-showcase/index.ts
+++ lib/generative-ai-showcase/index.ts
@@ -1,6 +1,9 @@
 import { EksBlueprint } from "@aws-quickstart/eks-blueprints";
 import * as blueprints from "@aws-quickstart/eks-blueprints";
+import * as spi from '@aws-quickstart/eks-blueprints/dist/spi';
+import * as utils from "@aws-quickstart/eks-blueprints/dist/utils";
 import { Construct } from "constructs";
+import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
 import { BedrockShowcaseAddon, BedrockShowcaseAddonProps } from "./bedrock-showcase-addon";
 export default class GenAIShowcase {
     constructor(scope: Construct, id: string) {
@@ -27,11 +30,35 @@ export default class GenAIShowcase {
                 new blueprints.addons.KubeProxyAddOn(),
                 new blueprints.addons.CertManagerAddOn(),
                 new blueprints.addons.KubeStateMetricsAddOn(),
+                new blueprints.addons.NestedStackAddOn({
+                    id: "usage-tracking-addon",
+                    builder: UsageTrackingAddOn.builder(),
+                }),
                 new blueprints.addons.SSMAgentAddOn(),
                 new blueprints.addons.MetricsServerAddOn(),
                 new BedrockShowcaseAddon(bedrockShowcaseAddonProps)
             )
             .build(scope, stackID);
 
+    }
+}
+
+/**
+ * Nested stack that is used as tracker for GPU Accelerator
+ */
+class UsageTrackingAddOn extends NestedStack {
+
+    static readonly USAGE_ID = "qs-1uijcfop9";
+
+    public static builder(): spi.NestedStackBuilder {
+        return {
+            build(scope: Construct, id: string, props: NestedStackProps) {
+                return new UsageTrackingAddOn(scope, id, props);
+            }
+        };
+    }
+
+    constructor(scope: Construct, id: string, props: NestedStackProps) {
+        super(scope, id, utils.withUsageTracking(UsageTrackingAddOn.USAGE_ID, props));
     }
 }
