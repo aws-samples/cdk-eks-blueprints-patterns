90141dc79b5fe8fb3c043018dbdf762aa2918d40
shapirov <shapirov@amazon.com>
2023-12-26 17:27:15 -0500

    Upgraded libraries, removed vulnerabilities, blueprints upgraded to 1.13.1

--- lib/rafay-construct/index.ts
+++ lib/rafay-construct/index.ts
@@ -5,18 +5,18 @@ import * as cdk from 'aws-cdk-lib';
 
 export default class RafayConstruct {
     async buildAsync(scope: cdk.App, id: string) {
-        await prevalidateSecrets(RafayConstruct.name, undefined, 'rafay-password-secret');
-        const stackId = `${id}-blueprint`;
-
         let rafayConfig = {
             organizationName: "rafay-eks-org-1", // replace with your organization Name
             email: "abc@example.com", // replace with your email
             firstName: "John", // replace with your first Name
             lastName: "Doe", // replace with your last Name
-            passwordSecret: "rafay-password-secret", // replace with a secret name in secrets manager that you have created
+            passwordSecret: "rafay-password-json", // replace with a secret name in secrets manager that you have created, must contain a single field "password"
             clusterName: "eks-cluster-1", // replace with the name that you want the cluster to be created in Rafay Console
             blueprintName: "minimal"
         } as rafayAddOn.RafayConfig;
+
+        await prevalidateSecrets(RafayConstruct.name, undefined, rafayConfig.passwordSecret!);
+        const stackId = `${id}-blueprint`;
 
         const addOns: Array<blueprints.ClusterAddOn> = [
             new rafayAddOn.RafayClusterAddOn(rafayConfig)
