8701849f40c006d0bfe48aa65e304513d3d933b9
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2024-02-16 10:01:36 -0800

    refactor to seperate builder with multiple AMP providers

---
+++ lib/multi-cluster-construct/multi-cluster-builder.ts
@@ -0,0 +1,62 @@
+import { Construct } from 'constructs';
+
+// Blueprints Lib
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+import * as amp from 'aws-cdk-lib/aws-aps';
+import { EksAnywhereSecretsAddon } from './eksa-secret-stores';
+
+
+export default class MultiClusterBuilderConstruct {
+    build(scope: Construct, id: string, workspaceName: string, account?: string, region?: string ) {
+        // Setup platform team
+        const accountID = account ?? process.env.CDK_DEFAULT_ACCOUNT! ;
+        const awsRegion =  region ?? process.env.CDK_DEFAULT_REGION! ;
+ 
+        const stackID = `${id}-blueprint`;
+        this.create(scope, workspaceName, accountID, awsRegion)
+            .build(scope, stackID);
+    }
+    
+
+    create(scope: Construct, workspaceName: string, account?: string, region?: string ) {
+        // Setup platform team
+        const accountID = account ?? process.env.CDK_DEFAULT_ACCOUNT! ;
+        const awsRegion =  region ?? process.env.CDK_DEFAULT_REGION! ;
+
+        const ampWorkspaceName = workspaceName;
+        const ampPrometheusEndpoint = (blueprints.getNamedResource(ampWorkspaceName) as unknown as amp.CfnWorkspace).attrPrometheusEndpoint;
+
+        return blueprints.ObservabilityBuilder.builder()
+            .account(accountID)
+            .region(awsRegion)
+            .enableOpenSourcePatternAddOns()
+            .withAmpProps({
+                ampPrometheusEndpoint: ampPrometheusEndpoint,
+            })
+            .resourceProvider(ampWorkspaceName, new blueprints.CreateAmpProvider(ampWorkspaceName, ampWorkspaceName))
+            .addOns(
+                new blueprints.addons.ExternalsSecretsAddOn(),
+                new blueprints.addons.FluxCDAddOn({
+                  version:"2.2.3",
+                  repositories:[{
+                       name: "eks-cloud-addons-conformance",
+                       namespace: "flux-system",
+                       repository: {
+                           repoUrl: 'https://github.com/aws-samples/eks-anywhere-addons',
+                           targetRevision: "main",
+                       },
+                       values: {
+                       },
+                       kustomizations: [
+                           {kustomizationPath: "./eks-anywhere-common/Addons/Core"},
+                           {kustomizationPath: "./eks-anywhere-common/Addons/Partner"}, 
+                           {kustomizationPath: "./eks-cloud/Partner"}, 
+                       ],
+                  }],
+                }),
+                new EksAnywhereSecretsAddon()
+            )
+    }
+}
+
+
--- lib/multi-cluster-construct/pipeline.ts
+++ lib/multi-cluster-construct/pipeline.ts
@@ -2,7 +2,7 @@ import * as blueprints from '@aws-quickstart/eks-blueprints';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-import { EksAnywhereSecretsAddon } from './eksa-secret-stores';
+import MultiClusterBuilderConstruct from './multi-cluster-builder';
 
 
 /**
@@ -11,44 +11,20 @@ import { EksAnywhereSecretsAddon } from './eksa-secret-stores';
 export class PipelineMultiCluster {
 
     async buildAsync(scope: Construct) {
-        // const context = await populateAccountWithContextDefaults();
-        const account = "810198167072";
-        const region = "us-east-2";
+        const accountID = process.env.CDK_DEFAULT_ACCOUNT! ;
+        const region = process.env.CDK_DEFAULT_REGION! ;
 
-        console.log(account,region)
         // environments IDs consts
         const X86_ENV_ID = `eks-x86-${region}`;
         const ARM_ENV_ID = `eks-arm-${region}`;
 
         const CLUSTER_VERSIONS = [
-            eks.KubernetesVersion.V1_24,
-            eks.KubernetesVersion.V1_25,
-            eks.KubernetesVersion.V1_26,
+            // eks.KubernetesVersion.V1_24,
+            // eks.KubernetesVersion.V1_25,
+            // eks.KubernetesVersion.V1_26,
             eks.KubernetesVersion.V1_27,
         ]
 
-        // const addons: Array<blueprints.ClusterAddOn> = [
-        //     new blueprints.addons.ExternalsSecretsAddOn(),
-        //     new blueprints.addons.FluxCDAddOn({
-        //       repositories:[{
-        //            name: "eks-cloud-addons-conformance",
-        //            namespace: "flux-system",
-        //            repository: {
-        //                repoUrl: 'https://github.com/aws-samples/eks-anywhere-addons',
-        //                targetRevision: "main",
-        //            },
-        //            values: {
-        //            },
-        //            kustomizations: [
-        //                {kustomizationPath: "./eks-anywhere-common/Addons/Core"},
-        //                {kustomizationPath: "./eks-anywhere-common/Addons/Partner"}, 
-        //                {kustomizationPath: "./eks-cloud/Addons/Core"}, 
-        //                {kustomizationPath: "./eks-cloud/Addons/Partner"}
-        //            ],
-        //       }],
-        //     }),
-        //   ]; 
-            
             let clusterProps : blueprints.MngClusterProviderProps = {
                 maxSize : 3,
                 minSize : 1,
@@ -58,67 +34,28 @@ export class PipelineMultiCluster {
             const stages : blueprints.StackStage[] = [];
 
             for(const version of CLUSTER_VERSIONS) {
+                const blueprint1 = new MultiClusterBuilderConstruct().create(scope,`X86-` + version.version.replace(".", "-"), accountID, region);
+
                 clusterProps.amiType = eks.NodegroupAmiType.AL2_X86_64;
                 clusterProps.instanceTypes  =  [ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.XLARGE)];
-                const blueprintX86 = blueprints.EksBlueprint.builder()
-                    .account(account)
+                const blueprintX86 = blueprint1
                     .version(version)
                     .clusterProvider(new blueprints.MngClusterProvider(clusterProps))
-                    .useDefaultSecretEncryption(true)
-                    .addOns( 
-                    new blueprints.addons.ExternalsSecretsAddOn(),
-                    new blueprints.addons.FluxCDAddOn({
-                      repositories:[{
-                           name: "eks-cloud-addons-conformance",
-                           namespace: "flux-system",
-                           repository: {
-                               repoUrl: 'https://github.com/aws-samples/eks-anywhere-addons',
-                               targetRevision: "main",
-                           },
-                           values: {
-                           },
-                           kustomizations: [
-                               {kustomizationPath: "./eks-anywhere-common/Addons/Core"},
-                               {kustomizationPath: "./eks-anywhere-common/Addons/Partner"}, 
-                               {kustomizationPath: "./eks-cloud/Addons/Core"}, 
-                               {kustomizationPath: "./eks-cloud/Addons/Partner"}
-                           ],
-                      }],
-                    })
-                    );
+                    .useDefaultSecretEncryption(true);
     
                 stages.push({
                     id: `${X86_ENV_ID}-` + version.version.replace(".", "-"),
                     stackBuilder : blueprintX86.clone(region)
                 });
-    
+
+                const blueprint2 = new MultiClusterBuilderConstruct().create(scope,`ARM-` + version.version.replace(".", "-"), accountID, region);
     
                 clusterProps.amiType = eks.NodegroupAmiType.AL2_ARM_64;
                 clusterProps.instanceTypes  =  [ec2.InstanceType.of(ec2.InstanceClass.M7G, ec2.InstanceSize.XLARGE)];
-                const blueprintARM = blueprints.EksBlueprint.builder()
-                    .account(account)
+                const blueprintARM = blueprint2
                     .version(version)
                     .clusterProvider(new blueprints.MngClusterProvider(clusterProps))
-                    .useDefaultSecretEncryption(true)
-                    .addOns( new blueprints.addons.ExternalsSecretsAddOn(),
-                    new blueprints.addons.FluxCDAddOn({
-                      repositories:[{
-                           name: "eks-cloud-addons-conformance",
-                           namespace: "flux-system",
-                           repository: {
-                               repoUrl: 'https://github.com/aws-samples/eks-anywhere-addons',
-                               targetRevision: "main",
-                           },
-                           values: {
-                           },
-                           kustomizations: [
-                               {kustomizationPath: "./eks-anywhere-common/Addons/Core"},
-                               {kustomizationPath: "./eks-anywhere-common/Addons/Partner"}, 
-                               {kustomizationPath: "./eks-cloud/Addons/Core"}, 
-                               {kustomizationPath: "./eks-cloud/Addons/Partner"}
-                           ],
-                      }],
-                    }));
+                    .useDefaultSecretEncryption(true);
                         
                 stages.push({
                     id: `${ARM_ENV_ID}-` + version.version.replace(".", "-"),
