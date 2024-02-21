dfd636df8b6479c46eb319c80663d6c0bd817773
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2024-02-20 17:42:15 -0800

    est

--- lib/multi-cluster-construct/pipeline.ts
+++ lib/multi-cluster-construct/pipeline.ts
@@ -102,7 +102,7 @@ export class PipelineMultiCluster {
                 .clone(region, accountID)
         });
 
-        const gitOwner = 'howlla';
+        const gitOwner = 'Howlla';
         const gitRepositoryName = 'cdk-eks-blueprints-patterns';
 
         blueprints.CodePipelineStack.builder()
