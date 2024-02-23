1432aacaf0f512264e8944ea594a683fa3fa3564
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2024-02-23 11:40:53 -0800

    amp resource provider

--- lib/multi-cluster-construct/grafana-monitor-builder.ts
+++ lib/multi-cluster-construct/grafana-monitor-builder.ts
@@ -23,14 +23,14 @@ export default class GrafanaMonitoringConstruct {
         const region = contextRegion! || process.env.COA_AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
 
         const ampWorkspaceName = "multi-cluster-monitoring";
-        const ampPrometheusEndpoint = (blueprints.getNamedResource(ampWorkspaceName) as unknown as amp.CfnWorkspace).attrPrometheusEndpoint;
+        const ampPrometheusEndpoint = (blueprints.getNamedResource(ampWorkspaceName) as unknown as amp.CfnWorkspace);
         const amgEndpointUrl = process.env.AMG_ENDPOINT_URL;
 
         Reflect.defineMetadata("ordered", true, blueprints.addons.GrafanaOperatorAddon); //sets metadata ordered to true for GrafanaOperatorAddon
 
         const fluxRepository: blueprints.FluxGitRepo = blueprints.utils.valueFromContext(scope, "fluxRepository", undefined);
         fluxRepository.values!.AMG_AWS_REGION = region;
-        fluxRepository.values!.AMP_ENDPOINT_URL = 'https://aps-workspaces.us-east-2.amazonaws.com/workspaces/ws-d0a9753e-7bec-492b-9ef9-21f26f9a9469/';
+        fluxRepository.values!.AMP_ENDPOINT_URL = ampPrometheusEndpoint.attrPrometheusEndpoint;
         fluxRepository.values!.AMG_ENDPOINT_URL = 'https://g-3030e8b08a.grafana-workspace.us-east-2.amazonaws.com';
 
         const addOns: Array<blueprints.ClusterAddOn> = [
