f885b53f4840b025244335fd14c04b691b221321
howlla <bhavye.sharma1996@gmail.com>
2024-03-22 11:57:07 -0700

    remove ssm agent

--- lib/multi-cluster-construct/multi-cluster-builder.ts
+++ lib/multi-cluster-construct/multi-cluster-builder.ts
@@ -142,8 +142,7 @@ export default class MultiClusterBuilderConstruct {
                 }),
                 new EksAnywhereSecretsAddon(),
                 new blueprints.addons.EbsCsiDriverAddOn(),
-                new blueprints.addons.ClusterAutoScalerAddOn(),
-                new blueprints.addons.SSMAgentAddOn()
+                new blueprints.addons.ClusterAutoScalerAddOn()
             );
     }
 }
