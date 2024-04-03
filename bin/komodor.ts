96234c1563098c49c87dac29e056791cad4e8130
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-04-02 21:22:22 -0400

    Komodor Fix

--- bin/komodor.ts
+++ bin/komodor.ts
@@ -1,6 +1,6 @@
 import KomodorConstruct from '../lib/komodor-construct';
 import { configureApp } from '../lib/common/construct-utils';
 
-const app = configureApp();
+// const app = configureApp();
 
-new KomodorConstruct(app, 'komodor');
+// new KomodorConstruct(app, 'komodor');
