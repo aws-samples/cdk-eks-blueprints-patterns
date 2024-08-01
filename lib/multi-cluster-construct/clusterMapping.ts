3b873e04e07522834c922341dec64be58a18054b
Bhavye Sharma <bhavye.sharma1996@gmail.com>
2024-08-01 02:12:24 -0700

    Conformitron with cost optimizations (#176)
    * initial commit
    
    * remove argocd repo code
    
    * argoCD Infra and Flux  GitOps
    
    * update application and branch
    
    * update argo addon config owner
    
    * update owner of github argo addon
    
    * creating addons inside for loop
    
    * moving addons creation inside for loop
    
    * update branch of argo addon
    
    * update branch for argo addon
    
    * update flux
    
    * 8 clusters enabled
    
    * sepearte addons
    
    * refactor to seperate builder with multiple AMP providers
    
    * external secrets already part of oss pattern
    
    * Bottlerocket clusters added, eks 1.24 and 1.25 support removed
    
    * coredns version explicitly passed to stop fail for eks1.26
    
    * reorder addon props to pass them correctly
    
    * documentation added
    
    * mkdocs fix
    
    * mkdocs update
    
    * mkdocs fix formatting for service quotas
    
    * test
    
    * est
    
    * hardcode amg
    
    * upbound
    
    * downgrade eks for grafana cluster
    
    * hardcode amp amg
    
    * update https
    
    * ssm agent addon
    
    * ssm agent
    
    * Add files via upload
    
    * secrets update
    
    * docs update
    
    * amp resource provider
    
    * modify kustomizations
    
    * amgupdate
    
    * update branch
    
    * ebs-csi-driver added
    
    * ebs-csi-driver added
    
    * Add cluster autoscaler
    
    * minify bottlerocket clusterName
    
    * reduce metrics collected
    
    * add autoscaler
    
    * Add csi driver
    
    * add resources
    
    * testing
    
    * modify scrape rules
    
    * Seperate BR logs
    
    * remove amp for grafana cluster
    
    * kustomizations added and nodexp monitoring removed
    
    * further cost optimize
    
    * cost optimize 2xlarge
    
    * modify nodegroup names
    
    * cost optimization
    
    * change branch for flux
    
    * change branch to howlla
    
    * reset branch
    
    * remove ssm agent
    
    * force single node only
    
    * update to max 2
    
    * remove testers
    
    * update disksize
    
    * rename mng node group
    
    * reduce number of clusters
    
    * single AMP
    
    * single AMP
    
    * single AMP
    
    * single amp try2
    
    * single amp try3
    
    * using export variable for ampProvider
    
    * attempt 5
    
    * GetNamedResource to get the ampProvider
    
    * GetNamedResource to get the ampProvider
    
    * HardCode the endpoint
    
    * HardCode the endpoint
    
    * remove repeated scraping rules from amp
    
    * change monitoring code
    
    * change monitoring code
    
    * useopensourcepattern addons"
    
    * remove repeated addons"
    
    * reorder resource provider
    
    * hardcode workspace
    
    * all clusters go
    
    * final commit
    
    * conformitronPipeline
    
    * lint fix
    
    * remove coredns addon version
    
    * fix kubeflow link to stop .md link error
    
    * improve documentation and remove cdk variables
    
    * refactoring code as per feedback received on PR
    
    * integrate Mikhail's feedback
    
    * integrate feedback
    
    * add dashboards
    
    * minor typo in instructions and functional testing
    
    * remove extra commited files
    
    * fix org name for testing

---
+++ lib/multi-cluster-construct/clusterMapping.ts
@@ -0,0 +1,44 @@
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+/**
+ * Instance Mapping for fields such as chart, version, managed IAM policy.
+ */
+export interface InstanceMapping {
+    amiType: eks.NodegroupAmiType,
+    instanceType: ec2.InstanceType,
+}
+/**
+ * List of all clusters deployed by conformitron
+ */
+export enum ClusterName {
+  ARM = "arm",
+  X86 = "x86",
+  BR_X86 = "br-x86",
+  BR_ARM = "br-arm",
+  MONITORING = "grafana-monitoring"
+}
+
+
+export const clusterMappings : {[key in ClusterName]?: InstanceMapping } = {
+    [ClusterName.ARM]: {
+        amiType: eks.NodegroupAmiType.AL2_ARM_64,
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.M7G, ec2.InstanceSize.XLARGE2)
+    },
+    [ClusterName.X86]: {
+        amiType: eks.NodegroupAmiType.AL2_X86_64,
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.XLARGE2)
+    },
+    [ClusterName.BR_ARM]: {
+        amiType: eks.NodegroupAmiType.BOTTLEROCKET_ARM_64,
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.M7G, ec2.InstanceSize.XLARGE2)
+    },
+    [ClusterName.BR_X86]: {
+        amiType: eks.NodegroupAmiType.BOTTLEROCKET_X86_64,
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.XLARGE2)
+    },
+    [ClusterName.MONITORING]: {
+        amiType: eks.NodegroupAmiType.AL2_X86_64,
+        instanceType:  ec2.InstanceType.of(ec2.InstanceClass.M5, ec2.InstanceSize.LARGE)
+    }
+};
---
+++ lib/multi-cluster-construct/grafana-monitor-builder.ts
@@ -0,0 +1,126 @@
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import { GrafanaOperatorSecretAddon } from './grafana-operator-secret-addon';
+import * as fs from 'fs';
+
+export class GrafanaMonitoringConstruct {
+
+    build(scope: Construct, id: string, contextAccount?: string, contextRegion?: string ) {
+
+        const stackId = `${id}-grafana-monitor`;
+
+        const account = contextAccount! || process.env.ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
+        const region = contextRegion! || process.env.AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
+
+        this.create(scope, account, region)
+            .build(scope, stackId);
+    }
+
+    create(scope: Construct, contextAccount?: string, contextRegion?: string ) {
+
+        const account = contextAccount! || process.env.ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
+        const region = contextRegion! || process.env.AWS_REGION! || process.env.CDK_DEFAULT_REGION!;
+        
+        // TODO: CFN import https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.Fn.html#static-importwbrvaluesharedvaluetoimport
+        const ampWorkspaceName = "conformitronWorkspace";
+        const ampEndpoint = blueprints.utils.valueFromContext(scope, "conformitron.amp.endpoint", "https://aps-workspaces.<region>.amazonaws.com/workspaces/<workspace-id>/");
+        const ampWorkspaceArn = blueprints.utils.valueFromContext(scope, "conformitron.amp.arn", "arn:aws:aps:<region>:<accountid>:workspace/<workspace-id>");
+        
+        const ampAddOnProps: blueprints.AmpAddOnProps = {
+            ampPrometheusEndpoint: ampEndpoint,
+            ampRules: {
+                ampWorkspaceArn: ampWorkspaceArn,
+                ruleFilePaths: [
+                    __dirname + '/resources/amp-config/alerting-rules.yml',
+                    __dirname + '/resources/amp-config/recording-rules.yml'
+                ]
+            }
+        };
+
+        let doc = blueprints.utils.readYamlDocument(__dirname + '/resources/otel-collector-config.yml');
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableJavaMonJob }}",
+            "{{ stop enableJavaMonJob }}",
+            false
+        );
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableNginxMonJob }}",
+            "{{ stop enableNginxMonJob }}",
+            false
+        );
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableIstioMonJob }}",
+            "{{ stop enableIstioMonJob }}",
+            false
+        );
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableAPIserverJob }}",
+            "{{ stop enableAPIserverJob }}",
+            false
+        );
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableAdotMetricsCollectionJob}}",
+            "{{ stop enableAdotMetricsCollectionJob }}",
+            false
+        );
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableAdotMetricsCollectionTelemetry }}",
+            "{{ stop enableAdotMetricsCollectionTelemetry }}",
+            true
+        );
+
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableAdotContainerLogsReceiver }}",
+            "{{ stop enableAdotContainerLogsReceiver }}",
+            true
+        );
+        doc = blueprints.utils.changeTextBetweenTokens(
+            doc,
+            "{{ start enableAdotContainerLogsExporter }}",
+            "{{ stop enableAdotContainerLogsExporter }}",
+            true
+        );
+
+        fs.writeFileSync(__dirname + '/resources/otel-collector-config-new.yml', doc);
+
+        ampAddOnProps.openTelemetryCollector = {
+            manifestPath: __dirname + '/resources/otel-collector-config-new.yml',
+            manifestParameterMap: {
+                logGroupName: `/aws/eks/conformitron/workspace`,
+                logStreamName: `$NODE_NAME`,
+                logRetentionDays: 30,
+                awsRegion: region 
+            }
+        };
+
+        const fluxRepository: blueprints.FluxGitRepo = blueprints.utils.valueFromContext(scope, "fluxRepository", undefined);
+        fluxRepository.values!.AMG_AWS_REGION = region;
+        fluxRepository.values!.AMG_ENDPOINT_URL = blueprints.utils.valueFromContext(scope, "conformitron.amg.endpoint","https://<grafana-id>.grafana-workspace.<region>.amazonaws.com"); 
+
+        Reflect.defineMetadata("ordered", true, blueprints.addons.GrafanaOperatorAddon); //sets metadata ordered to true for GrafanaOperatorAddon
+        const addOns: Array<blueprints.ClusterAddOn> = [
+            new blueprints.addons.FluxCDAddOn({"repositories": [fluxRepository]}),
+            new GrafanaOperatorSecretAddon(),
+            new blueprints.addons.SSMAgentAddOn()
+        ];
+
+        return blueprints.ObservabilityBuilder.builder()
+            .account(account)
+            .region(region)
+            .version(eks.KubernetesVersion.V1_27)
+            .resourceProvider(ampWorkspaceName, new blueprints.CreateAmpProvider(ampWorkspaceName, ampWorkspaceName))
+            .withAmpProps(ampAddOnProps)
+            .enableOpenSourcePatternAddOns()
+            .addOns(
+                ...addOns
+            );
+    }
+}
