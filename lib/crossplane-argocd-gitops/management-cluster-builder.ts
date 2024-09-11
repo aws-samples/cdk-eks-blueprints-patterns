d0d66152e22d5ee9eb65cacb78999fc55b7a688c
jalawala <jalawala@amazon.com>
2024-09-11 09:40:11 +0530

    removing secret store secret

--- lib/crossplane-argocd-gitops/management-cluster-builder.ts
+++ lib/crossplane-argocd-gitops/management-cluster-builder.ts
@@ -6,7 +6,7 @@ import { UpboundCrossplaneAddOn } from './custom-addons/upbound-crossplane-addon
 import  { UpboundCrossplaneEKSProviderAddOn } from './custom-addons/upbound-crossplane-eks-provider-addon';
 import  { CrossplaneK8sProviderAddon } from './custom-addons/crossplane-k8s-provider-addon';
 import  { CrossplaneHelmProviderAddon } from './custom-addons/crossplane-helm-provider-addon';
-import { TeamSpoc } from './custom-addons/secret-provider-secret';
+// import { TeamSpoc } from './custom-addons/secret-provider-secret';
 
 
 const gitUrl = 'https://github.com/ajpaws/eks-blueprints-workloads.git';
@@ -38,9 +38,7 @@ export default class ManagementClusterBuilder {
                 bootstrapRepo: {
                     repoUrl: gitUrl,
                     path: `./crossplane-argocd-gitops/envs/dev`,
-                    targetRevision: 'main',
-                    credentialsSecretName: 'github-token',
-                    credentialsType: 'TOKEN'
+                    targetRevision: 'main'
                 },
                 bootstrapValues: {
                     clusterA: {
@@ -69,8 +67,7 @@ export default class ManagementClusterBuilder {
             .version(eks.KubernetesVersion.V1_29)
             .enableNativePatternAddOns()
             .enableControlPlaneLogging()
-            .addOns(...addOns)
-            .teams(new TeamSpoc());
+            .addOns(...addOns);
     }
 }
 
