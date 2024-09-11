c8784376248249dc164dd75ccf900dadfce32b88
jalawala <jalawala@amazon.com>
2024-09-11 22:26:55 +0530

    testing IRSA roles

--- lib/crossplane-argocd-gitops/custom-addons/upbound-crossplane-eks-provider-addon.ts
+++ lib/crossplane-argocd-gitops/custom-addons/upbound-crossplane-eks-provider-addon.ts
@@ -40,9 +40,9 @@ export class UpboundCrossplaneEKSProviderAddOn implements blueprints.ClusterAddO
                 ]
             })}));
 
-        clusterInfo.addAddOnContext(UpboundCrossplaneEKSProviderAddOn.name, {
-            arn: sa.role.roleArn
-        });
+        // clusterInfo.addAddOnContext(UpboundCrossplaneEKSProviderAddOn.name, {
+        //     arn: sa.role.roleArn
+        // });
 
         const runtimeConfig = new eks.KubernetesManifest(clusterInfo.cluster.stack, "runtimeConfig", {
             cluster: cluster,
@@ -88,7 +88,7 @@ export class UpboundCrossplaneEKSProviderAddOn implements blueprints.ClusterAddO
             ],
         });
         
-        runtimeConfig.node.addDependency(sa);
+        // runtimeConfig.node.addDependency(sa);
         awsEksProvider.node.addDependency(runtimeConfig);
         return Promise.resolve(runtimeConfig);
     }
