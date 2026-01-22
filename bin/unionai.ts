f3ebd4d400e93bbd4dd2ca14ce236f0acb091a2b
Zachary Jacobson <zjacobso@amazon.com>
2026-01-22 15:05:08 -0800

    add union pattern

---
+++ bin/unionai.ts
@@ -0,0 +1,6 @@
+import UnionDataplaneConstruct from '../lib/union-dataplane-construct'; '../lib/union-dataplane-construct/';
+import { configureApp } from '../lib/common/construct-utils';
+
+const app = configureApp();
+
+new UnionDataplaneConstruct().buildAsync(app, 'union-ai-datplane');
