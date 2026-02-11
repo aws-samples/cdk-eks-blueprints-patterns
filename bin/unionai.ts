deeb4c7578006832c354338a3c83d81c7b713e36
Zachary Jacobson <zjacobso@amazon.com>
2026-02-11 11:05:46 -0800

    fix lint

--- bin/unionai.ts
+++ bin/unionai.ts
@@ -4,6 +4,5 @@ import { configureApp, errorHandler } from '../lib/common/construct-utils';
 const app = configureApp();
 
 new UnionDataplaneConstruct().buildAsync(app, 'union-ai-datplane').catch((e) => {
-  errorHandler(app, "Union Dataplane Construct pattern is not setup due to missing secrets for Union client. See Union Dataplane Construct in the readme for instructions", e);
+    errorHandler(app, "Union Dataplane Construct pattern is not setup due to missing secrets for Union client. See Union Dataplane Construct in the readme for instructions", e);
 });
-;
