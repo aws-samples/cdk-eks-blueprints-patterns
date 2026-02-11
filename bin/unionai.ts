1d2aee547686d40730bc80c1b9d87183ca169668
Zachary Jacobson <zjacobso@amazon.com>
2026-02-11 11:02:51 -0800

    handle creds error

--- bin/unionai.ts
+++ bin/unionai.ts
@@ -1,6 +1,9 @@
 import UnionDataplaneConstruct from '../lib/union-dataplane-construct';
-import { configureApp } from '../lib/common/construct-utils';
+import { configureApp, errorHandler } from '../lib/common/construct-utils';
 
 const app = configureApp();
 
-new UnionDataplaneConstruct().buildAsync(app, 'union-ai-datplane');
+new UnionDataplaneConstruct().buildAsync(app, 'union-ai-datplane').catch((e) => {
+  errorHandler(app, "Union Dataplane Construct pattern is not setup due to missing secrets for Union client. See Union Dataplane Construct in the readme for instructions", e);
+});
+;
